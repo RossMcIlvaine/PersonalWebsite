3cf0361ad609cbf66ff8bb820ab3b437300cf97f
Ross McIlvaine <rossmcilvaine@gmail.com>
2021-10-13 16:18:40 -0400

    Added slot to half-screen

--- src/App.svelte
+++ src/App.svelte
@@ -14,25 +14,27 @@
 	<InfoBlock title={"University of Notre Dame"} location={"Notre Dame, IN"} date={"Graduation Date: May 2022"} items_present={false} alt={"University of Notre Dame Logo"} src={NDsrc}/>
 
 	<h2>Experience</h2>
-	<InfoBlock title={"Marketing Site Development"} location={"Rochester, NY (Remote)"} date={"June 2021 - August 2021"}
-		items={["Launched marketing website redesign from scratch using HTML, CSS, and Javascript inside a Svelte application",
-				"Created and managed backend for form submission using AWS DynamoDB, Lambda, and API Gateway",
-				"Communicated closely with UI/UX designer and other engineers to build site to design specifications",
-				"Worked alongside engineering team in Scrumban development environment",
-				"Showcased website advancement to wider organization in multiple team demonstrations"]}/>
 
-	<InfoBlock title={"Campus Wayzz Mobile Application"} location={"Notre Dame, IN"} date={"Spring 2021"}
-		items={["Designed UI/UX for campus mapping application",
-				"Built frontend screens for app from scratch using JavaScript and the JavaScript React library",
-				"Linked user profile information to backend MongoDB database"]}/>
+	<HalfScreen block_type={'resume'}>
+		<InfoBlock title={"Marketing Site Development"} location={"Rochester, NY (Remote)"} date={"June 2021 - August 2021"}
+			items={["Launched marketing website redesign from scratch using HTML, CSS, and Javascript inside a Svelte application",
+					"Created and managed backend for form submission using AWS DynamoDB, Lambda, and API Gateway",
+					"Communicated closely with UI/UX designer and other engineers to build site to design specifications",
+					"Worked alongside engineering team in Scrumban development environment",
+					"Showcased website advancement to wider organization in multiple team demonstrations"]}/>
 
-	<InfoBlock title={"Tweet Classification"} location={"Notre Dame, IN"} date={"Fall 2020"}
-		items={["Collaborated with team members to identify a twitter user’s political affiliation using twitter data",
-				"Assembled relevant datasets and produced cleaned data sets for ease of use in modeling",
-				"Incorporated decision tree algorithms for tweet classification using Python"]}/>
+		<InfoBlock title={"Campus Wayzz Mobile Application"} location={"Notre Dame, IN"} date={"Spring 2021"}
+			items={["Designed UI/UX for campus mapping application",
+					"Built frontend screens for app from scratch using JavaScript and the JavaScript React library",
+					"Linked user profile information to backend MongoDB database"]}/>
+
+		<InfoBlock title={"Tweet Classification"} location={"Notre Dame, IN"} date={"Fall 2020"}
+			items={["Collaborated with team members to identify a twitter user’s political affiliation using twitter data",
+					"Assembled relevant datasets and produced cleaned data sets for ease of use in modeling",
+					"Incorporated decision tree algorithms for tweet classification using Python"]}/>
+	</HalfScreen>
 
 	<HalfScreen/>
-	<HalfScreen flipped={true}/>
 	<Footer/>
 </main>
 
--- src/Components/HalfScreen.svelte
+++ src/Components/HalfScreen.svelte
@@ -1,14 +1,21 @@
 <script>
-    export let flipped = false;
+    export let block_type = 'default';
 </script>
 
 <main>
-    {#if flipped}
+    {#if block_type=='default'}
         <div class='half2'>
 
         </div>
         <div class='half1'>
 
+        </div>
+    {:else if block_type=='resume'}
+        <div class='half1'>
+
+        </div>
+        <div class='half2'>
+            <slot></slot>
         </div>
     {:else}
         <div class='half1'>
@@ -37,5 +44,10 @@
         height: 100%;
         width: 50%;
         background-color: white;
+        overflow-y: scroll;
+    }
+    /* Hide scrollbar in section */
+    .half2::-webkit-scrollbar {
+        display: none;
     }
 </style>
